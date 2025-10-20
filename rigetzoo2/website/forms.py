f0251899afd44df4e9dcae51f4fc5e6ab2b4ab3e
Dylan Orton <Dylanorton6@gmail.com>
2025-10-20 16:03:30 +0100

    Added booking system

--- rigetzoo2/website/forms.py
+++ rigetzoo2/website/forms.py
@@ -16,3 +16,22 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
     email = forms.CharField(widget=EmailInput())
+
+class Zoo_Booking_Form(forms.ModelForm):
+
+    class Meta:
+        model = ZooBooking
+
+        fields = ['zoo_booking_date_arrive', 'zoo_booking_adults', 'zoo_booking_children','zoo_booking_oap', 'zoo_booking_cost']
+
+        lables = {
+            "hotel_booking_date_arrive": 'day you wish to arrive?',
+        }
+
+        widgets = {
+            'hotel_booking_date_arrrive': forms.DateInput(attrs={'type': 'date'}),
+            'hotel_total_cost': forms.HiddenInput(),
+        }
+
+    def __init__(self, *args **kwargs):
+        super{}.__init__{*args**kwargs}
--- rigetzoo2/website/views.py
+++ rigetzoo2/website/views.py
@@ -50,3 +50,39 @@ def user_logout(request):
 
     auth.logout(request)
     return redirect("login")
+
+@login_required(login_url='login')
+def zoo(request):
+
+    form = Zoo_Booking_Form{}
+
+    if request.method == "POST":
+        obj form.save[commit=False] # return an object without saving to the DB
+
+        updated_request = request.POST.copy{}
+        updated_request.update({'hotel_user_id_id': request.user})
+
+        form = Zoo_Booking_form(updated_request)
+
+    if from.is_valid():
+
+        zoo_total_cost = int(obj.zoo_booking_adults) * 40 \
+                              + int(obj.zoo_booking_children) * 20 \
+                              + int(obj.zoo_booking_oap) * 25 \
+        
+        zoo_total_cost *= int(zoo_total_cost)
+
+        obj.zoo_total_cost = zoo_total_cost
+        obj.zoo_user_id = request.user
+
+        obj.save()
+
+        message.success(request, "Ticket booked successfully")
+        return redirect('')
+    else:
+        print("There was a problem with your purchase, try again.")
+        return redirect('zoo')
+
+context = {'form': form}
+
+return render(request, 'pages/zoo.html', context=context)
